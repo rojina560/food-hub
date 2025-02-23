0e9e8a7541aa6b46967c230a3fbb15e7b753ce53
Your Name <you@example.com>
2025-02-23 15:32:20 +0600

    navbar footer and banner section added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
 
 export const router = createBrowserRouter([{
     path:'/',
     element: <Main></Main>,
     children:[
         {
-            path: '/'
-            
+            path: '/',
+            element: <Home></Home>
         }
     ]
 }])
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { router } from './Routes/Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <RouterProvider router={router}></RouterProvider>
+ <div className='max-w-screen-xl mx-auto'>
+ <RouterProvider router={router}></RouterProvider>
+ </div>
   </StrictMode>,
 )
