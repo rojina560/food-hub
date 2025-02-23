942592e36ee87ceb90ab70313592d87d67547083
Your Name <you@example.com>
2025-02-23 11:12:32 +0600

    project initial setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+
+export const router = createBrowserRouter([{
+    path:'/',
+    element: <Main></Main>,
+    children:[
+        {
+            path: '/'
+            
+        }
+    ]
+}])
